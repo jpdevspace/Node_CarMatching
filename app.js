9794247f7311af1c4205f7b38ead341cf0e7ba9e
jpwebdev <jjpwebdev@gmail.com>
2018-01-18 07:56:31 -0500

    Home Route and Survey Route done

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
+// Requiring Modules and Aligning Satellites
 const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
 
-
+// Initiating our Express App
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello World!'));
+// Body-Parser Middleware: Parse incoming requests
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
+// Serve static files from /public
+app.use(express.static(__dirname + '/public'));
+
+//Home Route (NOT really needed, since I'm serving all static files from /public)
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+app.get('/survey', (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/survey.html"));
+})
+
+// Initiating Server
+app.listen(3000, () => console.log('App listening on port 3000!'));
