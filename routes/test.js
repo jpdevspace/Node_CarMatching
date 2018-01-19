32b1e5d35e446ab24903636c3fc366a59206e565
jpwebdev <jjpwebdev@gmail.com>
2018-01-19 00:01:54 -0500

    Comments added to algorithm in test.js

--- routes/test.js
+++ routes/test.js
@@ -101,21 +101,24 @@ const userData =  {
     }
 
 
-let diffArr = [];
-let result = 0;
-let resultArr = [];
+let eachDiffArr = [];   // Holds the difference every user and data (userData.roadtrip - carData.roadtrip... for every property)
+let totalSumOfDiff = 0; // Holds the total sum of the difference for every car in our data
+let finalResultArr = [];    // Holds all total sums of all the differences between the user's values and our cars
 
+// Loop through our carData array
 for (let i = 0; i < carData.length; i++) {
-    
+    // Loop through the properties in the user's data
     for (let key in userData) {
-
-        diffArr.push( Math.abs(userData[key] - carData[i][key]) ); 
-
+        // Get the difference between the user's point for a property and the same property in current iteration's car in our data
+        eachDiffArr.push( Math.abs(userData[key] - carData[i][key]) ); 
     }
-
-    result = diffArr.reduce( (total, subject) => total + subject, 0);
-    resultArr.push(result);
-    diffArr = [];
+    // When it finishes looping through all the first car's properties, getting the difference on every property
+    // Sum all those differences 
+    totalSumOfDiff = eachDiffArr.reduce( (total, points) => total + points, 0);
+    // Push the value to an array
+    finalResultArr.push(totalSumOfDiff);
+    // Clear the array to start pushing the differences between the user's data and the next car
+    eachDiffArr = [];
 }
 
-console.log(resultArr);
+console.log(finalResultArr);
